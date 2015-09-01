5b0e8b7544b9c5e3318d4d7c591abd28a77ace4e
Fernando Flores <ferflores@gmail.com>
2015-09-01 06:32:36 -0500

    fb configuration in env vars

--- server/serviceConfs.js
+++ server/serviceConfs.js
@@ -4,6 +4,6 @@ ServiceConfiguration.configurations.remove({
  
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '858242467624545',
-    secret: '41f89cdce377704d680b771a2448c041'
+    appId: process.env.FB_APP_ID,
+    secret: process.env.FB_SECRET
 });
