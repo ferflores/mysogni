e4993925f48e2cbeee4068049a4cc5e9a64e086b
Fernando Flores <ferflores@gmail.com>
2015-10-04 18:09:15 -0500

    android images

---
+++ mobile-config.js
@@ -0,0 +1,25 @@
+App.info({
+	name: 'MySogni',
+	description: 'Dream research app',
+	version: '1.0.0'
+});
+
+App.icons({
+	// Android
+  'android_ldpi': 'public/resources/icons/icon-36x36.png',
+  'android_mdpi': 'public/resources/icons/icon-48x48.png',
+  'android_hdpi': 'public/resources/icons/icon-72x72.png',
+  'android_xhdpi': 'public/resources/icons/icon-96x96.png'
+});
+
+App.launchScreens({
+  // Android
+  'android_ldpi_portrait': 'public/resources/splash/splash-200x320.jpg',
+  'android_ldpi_landscape': 'public/resources/splash/splash-320x200.jpg',
+  'android_mdpi_portrait': 'public/resources/splash/splash-320x480.jpg',
+  'android_mdpi_landscape': 'public/resources/splash/splash-480x320.jpg',
+  'android_hdpi_portrait': 'public/resources/splash/splash-480x800.jpg',
+  'android_hdpi_landscape': 'public/resources/splash/splash-800x480.jpg',
+  'android_xhdpi_portrait': 'public/resources/splash/splash-720x1280.jpg',
+  'android_xhdpi_landscape': 'public/resources/splash/splash-1280x720.jpg'
+});
